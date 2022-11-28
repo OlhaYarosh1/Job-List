5837e7fe6b1cbd794b6c9a045835d314c2a20fcf
Olha Yarosh <yarosholha1@gmail.com>
2022-11-28 23:45:44 +0200

    Added upper part to design for Detailed job Page.

--- src/DetailedPage/DetailedJobContainer/DetailedJobComponent/DetailedJobComponent.js
+++ src/DetailedPage/DetailedJobContainer/DetailedJobComponent/DetailedJobComponent.js
@@ -6,22 +6,44 @@ const DetailedJobComponent = () => {
     return (
         <div className={classes.detailedPage}>
             <div className={classes.commonPart}> 
-                <div className={classes.upperPart}>
-                    <div className={classes.title}>Job Details</div>
-                    <div className={classes.saveToList}>
-                        <img src='icons/Save.png' alt='' className={classes.saveIcon}/>
-                        <div className={classes.saveTitle}>Save to my list</div>
-                    </div>
-                    <div className={classes.share}>
-                        <img src='icons/Share.png' alt='' className={classes.shareIcon}/>
-                        <div className={classes.shareTitle}>Share</div>
-                    </div>
-                    <hr className={classes.line}/>
-                    <Button
-                        className={classes.applyNow}
-                        title='Apply now'
-                    />
+                <div className={classes.title}>Job Details</div>
+                <div className={classes.saveToList}>
+                    <img src='icons/Save.png' alt='' className={classes.saveIcon}/>
+                    <div className={classes.saveTitle}>Save to my list</div>
                 </div>
+                <div className={classes.share}>
+                    <img src='icons/Share.png' alt='' className={classes.shareIcon}/>
+                    <div className={classes.shareTitle}>Share</div>
+                </div>
+                <hr className={classes.line}/>
+                <Button
+                    className={classes.applyNow}
+                    title='Apply now'
+                />
+                <div className={classes.compensation}>Compensation & Benefits:</div>
+                <Button
+                    className={classes.applyNowBottom}
+                    title='Apply now'
+                />
+                <div className={classes.additionalInfo}>Additional info</div>
+                <hr className={classes.lineAdditional}/>
+                <div className={classes.employmentTypeTitle}>Employment type</div>
+                <div className={classes.employmentTypeBox}>
+                    <div className={classes.employmentTitle}>Full time</div> //
+                </div>
+                <div className={classes.benefitsTitle}>Benefits</div>
+                <div className={classes.benefitsBox}>
+                    <div className={classes.benefitsBoxTitle}>Flexible shedule</div> 
+                </div>
+                <div className={classes.attachedImages}>Attached images</div>
+                <hr className={classes.lineImages}/>
+                <img src=''/>
+                <Button
+                    className={classes.return}
+                    title='Return to job board'
+                >
+                    <img src='icons/ButtonArrow' alt='' className={classes.arrowIcon}/>
+                </Button>
             </div>
             <div className={classes.individualPart}>
 
